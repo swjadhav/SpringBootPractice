655ebb7f5fc4de323f074157959cbd16223f75b2
Swapnil Jadhav <swapnil.j111@gmail.com>
2020-06-07 13:59:09 +0530

    Added new methods

--- src/main/java/com/springdemo/BaseBallCoach.java
+++ src/main/java/com/springdemo/BaseBallCoach.java
@@ -1,4 +1,7 @@
 package com.springdemo;
 
-public class BaseBallCoach {
+public class BaseBallCoach implements Coach{
+    public String getDailyWorkOut() {
+        return "Run for 30 mins daily";
+    }
 }
--- src/main/java/com/springdemo/MyApp.java
+++ src/main/java/com/springdemo/MyApp.java
@@ -1,4 +1,15 @@
 package com.springdemo;
 
 public class MyApp {
+
+
+
+    public  static void main(String[] args)
+    {
+        Coach mycoach = new BaseBallCoach();
+        System.out.println(mycoach.getDailyWorkOut());
+
+    }
+
+
 }
